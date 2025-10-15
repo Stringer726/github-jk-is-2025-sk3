bb35fc03d8e5a625f1ea1338894dc9597b0000bf
josef.kubrt <josef.kubrt@tul.cz>
2025-10-15 19:56:48 +0200

    #4

---
+++ IS Programy/program000a-zakladni-kod/Program.cs
@@ -0,0 +1,52 @@
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("******************************");
+    Console.WriteLine("***** Výpis číselné řady *****");
+    Console.WriteLine("******************************");
+    Console.WriteLine("******** Josef Kubrt ********");
+    Console.WriteLine("********* 15.10.2025 **********");
+    Console.WriteLine("******************************");
+    Console.WriteLine();
+
+    //Vstup čiselné hodnoty do programu - špatně řešený princip
+    //  Console.Write("Zadejte první číslo řady: ");
+    //  int first = int.Parse(Console.ReadLine());
+
+
+
+  //Vstup čiselné hodnoty do programu - lépe řešený princip
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu:");
+    
+
+
+}
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stisknete klávesu a");
+  again = Console.ReadLine();
+  //noinspection CSharpWarnings__CS8600
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+}
+
