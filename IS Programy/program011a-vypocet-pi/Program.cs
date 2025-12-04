72b72ebcb2221edb2ba91cc11922f9a345d5e24d
josef.kubrt <josef.kubrt@tul.cz>
2025-12-04 16:21:57 +0100

    #17 basic

---
+++ IS Programy/program011a-vypocet-pi/Program.cs
@@ -0,0 +1,36 @@
+﻿﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("************* Konvergentní řady  *************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("**************** Josef Kubrt *****************");
+    Console.WriteLine("**************** 4.12.2025 ******************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+}
